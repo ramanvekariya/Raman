4226232d9369c7e1d4c5a9cb017fc095b1a3e338
ramanvekariya <ramanprogramming@gmail.com>
2024-10-11 20:30:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿// Sample image data
+const images = [
+    { src: 'https://picsum.photos/200/300', alt: 'Image 1' },
+    { src: 'https://picsum.photos/200/301', alt: 'Image 2' },
+    { src: 'https://picsum.photos/200/302', alt: 'Image 3' },
+    // Add more images here...
+];
+
+
+// Load images into the image container
+const imageContainer = document.querySelector('.image-container');
+images.forEach((image) => {
+    const img = document.createElement('img');
+    img.src = image.src;
+    img.alt = image.alt;
+    imageContainer.appendChild(img);
+});
